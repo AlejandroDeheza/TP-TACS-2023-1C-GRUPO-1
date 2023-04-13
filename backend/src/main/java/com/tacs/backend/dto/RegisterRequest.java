77db0a59ad2640921cd3b8abe90baf511d192fdd
TianshuWang <wangtianshu17@gmail.com>
2023-04-13 14:14:15 -0300

    add schema to dtos

--- backend/src/main/java/com/tacs/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/tacs/backend/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.tacs.backend.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tacs.backend.utils.ConfirmedField;
 import com.tacs.backend.utils.ValidPassword;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,18 +18,23 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
     @JsonProperty("first_name")
     @NotBlank(message = "First name can not be blank")
+    @Schema(description = "User first name", example = "Juan")
     private String firstName;
     @JsonProperty("last_name")
     @NotBlank(message = "Last name can not be blank")
+    @Schema(description = "User last name", example = "Perez")
     private String lastName;
     @JsonProperty("username")
     @NotBlank(message = "Username can not be blank")
+    @Schema(description = "Username", example = "juan.perez")
     private String username;
     @JsonProperty("password")
     @NotBlank(message = "Password can not be blank")
     @ValidPassword
+    @Schema(description = "Password", example = "mksiug_865K")
     private String password;
     @JsonProperty("password_confirmation")
     @NotBlank(message = "Password confirmation can not be blank")
+    @Schema(description = "Confirmation password", example = "mksiug_865K")
     private String passwordConfirmation;
 }
