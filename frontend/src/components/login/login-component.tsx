3651017ffca1307bb06c4fac5e5601f7f6496a08
TianshuWang <wangtianshu17@gmail.com>
2023-04-26 17:45:55 -0300

    add authentication, all events api

---
+++ frontend/src/components/login/login-component.tsx
@@ -0,0 +1,52 @@
+import React, { useState, FormEvent } from 'react';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies'
+
+export default function LoginComponent(req: any, res: any) {
+  const router = useRouter();
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    await authenticate({ "username": username, "password": password })
+  }
+
+  async function authenticate(data: object) {
+    try {
+      const response = await fetch("http://localhost:8091/v1/auth/authentication", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      const userData = await response.json()
+      console.log(userData)
+
+      setCookie({ res }, 'jwt', userData.access_token, {
+        httpOnly: false,
+        secure: process.env.NODE_ENV !== 'development',
+        maxAge: 30 * 24 * 60 * 60,
+        path: '/',
+      });
+
+      router.push("/events")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username </label>
+      <input type="text" id="username" name="username" value={username} onChange={e => setUsername(e.target.value)} required />
+
+      <label htmlFor="password">Password </label>
+      <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+};
---
+++ frontend/src/pages/events/index.tsx
@@ -0,0 +1,12 @@
+import AllEventsComponent from "../../components/events/all-events-component";
+
+
+const AllEvents = () => {
+  return (
+    <div>
+      <AllEventsComponent />
+    </div>
+  )
+}
+
+export default AllEvents;
