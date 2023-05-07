e01773b648c95ef20539d13ef8fbfd276b67719e
TianshuWang <wangtianshu17@gmail.com>
2023-05-07 01:21:15 -0300

    json ignore

--- backend/src/main/java/com/tacs/backend/dto/EventOptionDto.java
+++ backend/src/main/java/com/tacs/backend/dto/EventOptionDto.java
@@ -4,25 +4,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.tacs.backend.model.Event;
-import com.tacs.backend.model.User;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
+
 
 @Data
 @Builder
@@ -44,6 +36,7 @@ public class EventOptionDto {
     private long voteQuantity;
 
     @JsonProperty("vote_users")
+    @JsonIgnore
     @Schema(description = "Event option vote users", hidden = true)
     private List<UserDto> voteUsers;
 
